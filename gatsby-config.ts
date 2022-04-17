61f2b20e7e5f61c801a533287b5026f11d1e74b2
Alex Sin <sinalexandru@gmail.com>
2022-04-17 12:02:13 +0300

    Initial project setup

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,17 +1,30 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from "gatsby"
+
+const contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+}
+
+const { spaceId, accessToken } = contentfulConfig
+if (!spaceId || !accessToken) {
+  throw new Error(
+    "Contentful spaceId and the access token need to be provided."
+  )
+}
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `The Habit Guide`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: "The Habit Guide",
+    description: "Ideas, hacks and habits",
   },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "PfbvF9pN2uueEwoBBiauBN6t7WEwZEQf15WASua4JVw",
-      "spaceId": "rvswedterhkl"
-    }
-  }, "gatsby-plugin-sass"]
-};
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: contentfulConfig,
+    },
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+  ],
+}
 
-export default config;
+export default config
